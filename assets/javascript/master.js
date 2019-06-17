8ead486242dfb282ee3cb8a859a4508849dbad7e
ilger <ilgerdepraetere@gmail.com>
2019-06-17 10:19:40 +0200

    add whitespaces and comments

--- assets/javascript/master.js
+++ assets/javascript/master.js
@@ -1,3 +1,8 @@
+
+
+
+
+// Global variables
 let winLose = document.getElementById('Win-Lose');
 let rock = document.getElementById('Rock');
 let paper = document.getElementById('Paper');
@@ -7,7 +12,8 @@ let reset = document.getElementById('Reset');
 let computer;
 let player;
 
+//addEventListener function
 let buttonEvent = (buttonId, executeFn) => buttonId.addEventListener('click', executeFn);
 
-
+// test buttonEvent fn
 buttonEvent(draw, () => console.log('draw button'));
