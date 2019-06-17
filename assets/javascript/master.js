b3478cfdae0a3ee74ff7e1a3ac2ef8f74a386343
ilger <ilgerdepraetere@gmail.com>
2019-06-17 09:55:14 +0200

    add all html .get.. variables

---
+++ assets/javascript/master.js
@@ -0,0 +1,6 @@
+let winLose = document.getElementById('Win-Lose');
+let rock = document.getElementById('Rock');
+let paper = document.getElementById('Paper');
+let scissors = document.getElementById('Scissors');
+let draw = document.getElementById('Draw');
+let reset = document.getElementById('Reset');
