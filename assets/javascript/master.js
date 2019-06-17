516f2d84f3bd213bc7b47f238e4893decb741a58
ilger <ilgerdepraetere@gmail.com>
2019-06-17 11:54:38 +0200

    add win lose tie variables

--- assets/javascript/master.js
+++ assets/javascript/master.js
@@ -9,8 +9,14 @@ let paper = document.getElementById('Paper');
 let scissors = document.getElementById('Scissors');
 let draw = document.getElementById('Draw');
 let reset = document.getElementById('Reset');
+
 let computer;
 let player;
+
+let win = () => winLose.innerHTML = 'A Win';
+let lose =() => winLose.innerHTML = 'A Loss';
+let tie = () => winLose.innerHTML = 'A Tie';
+
 
 //addEventListener function
 let buttonEvent = (buttonId, executeFn) => buttonId.addEventListener('click', executeFn);
